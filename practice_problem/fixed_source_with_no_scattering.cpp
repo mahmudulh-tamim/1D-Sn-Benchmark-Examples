d7a8b4326d7c194608d97851a8a778ca1d17c6ba
MH Tamim <tamimibnahmed@gmail.com>
2024-08-30 23:50:23 -0400

    print correct

--- practice_problem/fixed_source_with_no_scattering.cpp
+++ practice_problem/fixed_source_with_no_scattering.cpp
@@ -60,7 +60,7 @@ int main()
                     psi_in[0][ang_count]=0;
                     psi_out[0][ang_count]=(2*q*del_x-psi_in[0][ang_count]*(sigma_t*del_x-2*angl[ang_count][0]))/(sigma_t*del_x+2*angl[ang_count][0]);
                     psi_avg[c_count][ang_count]=(psi_in[c_count][ang_count]+psi_out[c_count][ang_count])/2;
-                    std::cout<<"Cell:"<<c_count<<"\n"<<"Angle:"<<ang_count<<"\n"<<psi_in[c_count][ang_count]<<"\n"<<psi_in[c_count][ang_count]<<"\n"<<psi_avg[c_count][ang_count]<<"\n";
+                    std::cout<<"Cell:"<<c_count<<"\n"<<"Angle:"<<ang_count<<"\n"<<psi_in[c_count][ang_count]<<"\n"<<psi_out[c_count][ang_count]<<"\n"<<psi_avg[c_count][ang_count]<<"\n";
                 }
                 else
                 {
